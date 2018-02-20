dbbe1d870cc5e3da582da9041c5c48501e0c8a28
qinjie <mark.qj@gmail.com>
2018-02-21 05:34:53 +0800

    Initial version

---
+++ Dockerfile
@@ -0,0 +1,46 @@
+FROM php:7.0-apache
+
+# Use custom php ini file
+COPY php.ini /usr/local/etc/php/
+
+# Install dependencies
+RUN apt-get update
+RUN apt-get install -y zip curl vim
+RUN apt-get install -y libmcrypt-dev
+RUN apt-get install -y libmagickwand-dev imagemagick
+RUN apt-get install -y libfreetype6-dev libjpeg62-turbo-dev libpng12-dev
+
+RUN apt-get install -y libicu-dev \
+    && docker-php-ext-configure intl \
+    && docker-php-ext-install intl
+RUN docker-php-ext-install pdo_mysql 
+RUN docker-php-ext-install mysqli 
+RUN docker-php-ext-install iconv
+RUN docker-php-ext-install zip
+RUN apt-get install libcurl3-dev libcurl4-openssl-dev \
+    && docker-php-ext-install curl 
+RUN docker-php-ext-configure gd --with-freetype-dir=/usr/include/ --with-jpeg-dir=/usr/include/ 
+RUN docker-php-ext-install -j$(nproc) gd
+
+RUN pecl install imagick
+RUN curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+
+# Enable apache mods.
+RUN a2enmod rewrite
+
+# Apache conf
+# to see live logs we do : docker logs -f [CONTAINER ID]
+# without the following line we get "AH00558: apache2: Could not reliably determine the server's fully qualified domain name"
+RUN echo "ServerName localhost" >> /etc/apache2/apache2.conf
+
+# autorise .htaccess files
+RUN sed -i '/<Directory \/var\/www\/>/,/<\/Directory>/ s/AllowOverride None/AllowOverride All/' /etc/apache2/apache2.conf
+
+# Package sites into image
+COPY ./html/ /var/www/html/
+RUN chown -R www-data:www-data /var/www/html/
+
+EXPOSE 80 443 22 3306
+
+ENTRYPOINT service apache2 restart && bash
+
---
+++ README.php
@@ -0,0 +1,71 @@
+
+# Docker Commands
+0. Copy yii2 project folders to `html` folder
+
+1. Build an image (with tag = php-yii2) where Dockerfile is in current directory.
+    `docker build -t php-yii2 .`
+
+2. MySQL is not included in image. Map ports to use external MySQL Server
+    `docker run --rm -p 80:80 -p 3306:3306 --name my-php-yii2 -ti php-yii2 bash`
+
+3. For local testing, map local Sites folder to docker container
+    `docker run --rm -p 80:80 -p 3306:3306 -v ~/Sites:/var/www/html --name my-php-yii2 -ti php-yii2
+    bash`
+
+4. Other useful docker commands
+    - Clean up all dangling docker
+    `docker system prune`
+
+    - Start terminal with current running container
+    `docker exec <container-id> -ti bash`
+
+# Other Notes
+
+To Enable clean URL in Yii2
+
+1. Add .htaccess file in web root folder (frontend/web and backend/web for advanced template; web folder for basic template).
+
+```
+RewriteEngine on
+# If a directory or a file exists, use it directly
+RewriteCond %{REQUEST_FILENAME} !-f
+RewriteCond %{REQUEST_FILENAME} !-d
+# Otherwise forward it to index.php
+RewriteRule . index.php
+```
+
+2. In Yii configuration file (main.php for advanced template, or web.php for basic template), set `enablePrettyUrl` to true, and set `showScriptName` to false.
+
+    ```
+    'urlManager' => [
+        'class' => 'yii\web\UrlManager',
+        // Disable index.php
+        'showScriptName' => false,
+        // Disable r= routes
+        'enablePrettyUrl' => true,
+        'rules' => array(
+            '<controller:\w+>/<id:\d+>' => '<controller>/view',
+            '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
+            '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
+        ),
+    ],
+    ```
+
+3. Enable mod_rewrite for Apache by running following on terminal.
+
+    ```
+    $ sudo a2enmod rewrite
+    ```
+
+4.  Enable apache2.conf
+
+    ```
+    $ vim /etc/apache2/apache2.conf
+    ```
+	
+    <Directory /var/www/>
+	Options Indexes FollowSymLinks
+	AllowOverride All
+	Require all granted
+    </Directory>
+
